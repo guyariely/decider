4f4b441785094daa7d6daeeb150fd758d2cd4261
Guy Arieli <guyarieli1997@gmail.com>
2019-06-09 21:56:25 +0300

    add server file

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-const pathPublic = path.join(__dirname, '..', 'public');
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
-app.use(express.static(pathPublic));
-
-app.listen(port, () => {
-  console.log('server is up');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.listen(port);
