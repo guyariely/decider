9a29af0a3a09b125ffb23cf6099f2924b8f9d9d0
Guy Arieli <guyarieli1997@gmail.com>
2019-10-12 22:25:56 +0300

    added placeholder to add option input

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,32 +1,37 @@
-import React from 'react';
+import React from "react";
 
 export default class AddOption extends React.Component {
   constructor(props) {
-     super(props);
-     this.addOption = this.addOption.bind(this);
-     this.state = {
+    super(props);
+    this.addOption = this.addOption.bind(this);
+    this.state = {
       error: undefined
-     };
+    };
   }
   addOption(e) {
     e.preventDefault();
-        
+
     let option = e.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option);
     this.setState(() => ({ error }));
 
-    e.target.elements.option.value = '';
+    e.target.elements.option.value = "";
   }
 
   render() {
     return (
       <div>
-        {this.state.error && <p className="add-option-error">{this.state.error}</p>}
+        {this.state.error && (
+          <p className="add-option-error">{this.state.error}</p>
+        )}
         <form className="add-option" onSubmit={this.addOption}>
-          <input className="add-option__input" placeholder="Add option" type="text" name="option" />
-          <button className="button button--add">
-            Add option
-          </button>
+          <input
+            className="add-option__input"
+            placeholder="Enter a new option"
+            type="text"
+            name="option"
+          />
+          <button className="button button--add">Add option</button>
         </form>
       </div>
     );
